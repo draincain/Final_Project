69bd4ed321a316ceb4ba9745b02a25b5e0e76c2a
draincain <adrian.c.cain@gmail.com>
2024-02-20 10:02:56 -0600

    Updated About page

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,18 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1>About Us</h1>
+      <h1>Circling the Drain</h1>
       <p>
-        This is the about page for our React application. Here you can find
-        information about our mission, vision, and the services we offer.
+        Adrian Cain (Drain) is a glitch artist, residing currently in Northwest
+        Arkansas. Practicing since 2015, he first became aware of glitch as a
+        form of art through the FB group, Glitch Request and GAC. He first
+        started glitching selfies with wordpad in 2015. Now he focuses on
+        surreal portraits and abstract glitchscapes, changing once identifiable
+        forms into something new. Subverted patterns and surreal figures created
+        in Blender are then subject to sonification through Audacity.
+        Databending any image format he can get his hands on, Drain creates
+        glitch collages to highlight various techniques.
       </p>
-      {/* Add more content about your application or organization here */}
     </div>
   );
 };
